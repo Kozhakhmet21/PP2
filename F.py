8598f521d39721682273ca78929c18f7c1d68e6b
Kozhakhmet21 <k_nagashybaiuly@kbtu.kz>
2021-02-10 09:59:12 +0600

    Tema 3

---
+++ F.py
@@ -0,0 +1,8 @@
+a=input()
+b=input()
+c=input()
+d=a**2+b**2
+if d!=c*c:
+    print("NO")
+else: 
+    print("YES")
---
+++ I.py
@@ -0,0 +1,10 @@
+a=input()
+b=input()
+c=input()
+d=input()
+if a==b and c==d:
+    print("NO")
+elif a==b and c>d:
+    print("YES")
+else:
+    print("YES")
